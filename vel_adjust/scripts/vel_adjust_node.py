9d15940d5818587b1fdc61b6a60fb80ba5ad1b3e
hyueyung <109084266+hyueyung@users.noreply.github.com>
2022-07-11 23:00:21 +0800

    Update vel_adjust_node.py

--- vel_adjust/scripts/vel_adjust_node.py
+++ vel_adjust/scripts/vel_adjust_node.py
@@ -15,32 +15,46 @@ pub_vel_rr = rospy.Publisher("/wheel_vel/rear_right_adj", Float64,
 	queue_size=10)
 
 
-# Callback function for front left
+
 def callback_fl(data):
+    """
+    Callback function for front left
+    Publish the adjusted velocity
+    """
     print("Velocity (Front-Left): %.4f" % data.data)
     # Calculate the new velocity front_left_adj and publish to the topic
     pub_vel_fl.publish(data.data/ACTUAL_FL)
 
 
-# Callback function for front right
+
 def callback_fr(data):
+    """
+    Callback function for front right
+    """
     print("Velocity (Front-Right): %.4f" % data.data)
     
 
-# Callback function for rear left
+
 def callback_rl(data):
+    """
+    Callback function for rear left
+    """
     print("Velocity (Rear-Left): %.4f" % data.data)
 	
 
-# Callback function for rear right
+
 def callback_rr(data):
+    """
+    Callback function for rear right
+    Publish the adjusted velocity
+    """
     print("Velocity (Rear_Right): %.4f" % data.data)
     # Calculate the new velocity rear_right_adj and publish to the topic
     pub_vel_rr.publish(data.data/ACTUAL_RR)
 
 
 def vel_adjust():
-	# Initialise node and subscribe to the topics in /wheel_vel
+    # Initialise node and subscribe to the topics in /wheel_vel
     rospy.init_node("vel_adjust_node", anonymous = True)
     rospy.Subscriber("/wheel_vel/front_left", Float64, callback_fl)
     rospy.Subscriber("/wheel_vel/front_right", Float64, callback_fr)
@@ -53,11 +67,11 @@ def vel_adjust():
 
 if __name__ == '__main__':
     try:
-    	# vel_adjust is the main function of this node.
-    	# it reads the velocity messages for all wheels (in no particular
-    	# order or frequency) for the 4WD robot. It also calculate and output
-    	# the adjusted velocity values for the front left and rear right
-    	# wheel.
+        # vel_adjust is the main function of this node.
+        # it reads the velocity messages for all wheels (in no particular
+        # order or frequency) for the 4WD robot. It also calculate and output
+        # the adjusted velocity values for the front left and rear right
+        # wheel.
         vel_adjust()
     except rospy.ROSInterruptException:
         pass
